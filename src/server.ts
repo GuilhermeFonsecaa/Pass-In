7e1bc3854e9734da34f82e1c764a6fafd58da2d9
Guilherme Fonseca <guilhermao1234@hotmail.com>
2024-04-11 00:16:34 -0300

    refactor: creating separate file for route

--- src/server.ts
+++ src/server.ts
@@ -3,61 +3,14 @@ import { serializerCompiler, validatorCompiler, ZodTypeProvider } from "fastify-
 import { z } from "zod"
 import { PrismaClient } from "@prisma/client"
 import { generateSlug } from "./utils/generate-slug";
+import { createEvent } from "./routes/create-event";
 
 const app = fastify()
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
-const prisma = new PrismaClient({
-    log: ['query']
-})
-
-app
-    .withTypeProvider<ZodTypeProvider>()
-    .post("/events", {
-        schema: {
-            body:
-                z.object({
-                    title: z.string().min(4),
-                    details: z.string().nullable(),
-                    maximumAttendees: z.number().int().positive().nullable()
-                }),
-
-            response: {
-                201: z.object({
-                    eventId: z.string().uuid(),
-                })
-            }
-        },
-    },
-
-        async (request, reply) => {
-            const { title, details, maximumAttendees } = (request.body) //validação dos dados 
-            const slug = generateSlug(title)
-            const eventWithSameSlug = await prisma.evemt.findUnique({
-                where: {
-                    slug
-                }
-            })
-
-            if (eventWithSameSlug !== null) {
-                throw new Error('Another event with same title already exists')
-            }
-
-            const event = await prisma.evemt.create({
-                data: {
-                    title: title,
-                    details: details,
-                    maximumAttendees: maximumAttendees,
-                    slug: slug,
-                }
-            })
-
-            return reply.status(201).send({ eventId: event.id })
-        })
-
-
+app.register(createEvent)
 
 app.listen({ port: 3333 }).then(() => {
     console.log('HTTP server running')
