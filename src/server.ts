74cd29040cd3e16c96f6e72a39851293462cf369
Guilherme Fonseca <guilhermao1234@hotmail.com>
2024-04-15 00:41:29 -0300

    feat: add get events route

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,8 @@
 import fastify from "fastify";
-import { serializerCompiler, validatorCompiler, ZodTypeProvider } from "fastify-type-provider-zod";
-import { z } from "zod"
-import { PrismaClient } from "@prisma/client"
-import { generateSlug } from "./utils/generate-slug";
+import { serializerCompiler, validatorCompiler} from "fastify-type-provider-zod";
 import { createEvent } from "./routes/create-event";
 import { registerForEvents } from "./routes/register-for-events";
+import { getEvent } from "./routes/get-event";
 
 const app = fastify()
 
@@ -13,6 +11,7 @@ app.setSerializerCompiler(serializerCompiler);
 
 app.register(createEvent)
 app.register(registerForEvents)
+app.register(getEvent)
 
 app.listen({ port: 3333 }).then(() => {
     console.log('HTTP server running')
