0e328ed375d8728868b4ac5a734c39c2d39df690
Guilherme Fonseca <guilhermao1234@hotmail.com>
2024-04-20 02:58:31 -0300

    feat: add check in route

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import { createEvent } from "./routes/create-event";
 import { registerForEvents } from "./routes/register-for-events";
 import { getEvent } from "./routes/get-event";
 import { getAttendeeBadge } from "./routes/get-attendee-badge";
+import { checkIn } from "./routes/check-in";
 
 const app = fastify()
 
@@ -14,6 +15,7 @@ app.register(createEvent)
 app.register(registerForEvents)
 app.register(getEvent)
 app.register(getAttendeeBadge)
+app.register(checkIn)
 
 app.listen({ port: 3333 }).then(() => {
     console.log('HTTP server running')
